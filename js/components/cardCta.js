dadff8573c82a3f123398c254932f6bfd0e8ce32
EuAndersonDev <andersonaugustorei1227@gmail.com>
2026-03-06 13:13:07 -0300

    feat, results screen

--- js/components/cardCta.js
+++ js/components/cardCta.js
@@ -1,15 +1,20 @@
 class CardCta extends HTMLElement {
     connectedCallback() {
+        // Obtém os atributos personalizáveis ou usa valores padrão
+        const titulo = this.getAttribute('titulo') || 'Pronto para o desafio?';
+        const descricao = this.getAttribute('descricao') || 'Clique no botão abaixo e comece seu quiz agora. É rápido, gratuito e muito divertido!';
+        const textoBotao = this.getAttribute('texto-botao') || 'Iniciar Quiz Agora';
+        const link = this.getAttribute('link') || '#';
+
         this.innerHTML = `
         <div class="cardcta">
-            <b class="heading-2">Pronto para o desafio?</b>
-            <div class="clique-no-boto">
-                Clique no botão abaixo e comece seu quiz agora. É rápido,<br />gratuito
-                e muito divertido!
-            </div>
-            <div class="link">
-                <b class="iniciar-quiz-agora">Iniciar Quiz Agora </b>
-            </div>
+            <h2 class="heading-2">${titulo}</h2>
+            <p class="text-button-cardCta">
+                ${descricao}
+            </p>
+            <button class="link-buttonCta" onclick="window.location.href='${link}'">
+                ${textoBotao}
+            </button>
         </div>
         `;
     }
